d9f6e7e53b876c6e828ba09f455473dd709adc64
Nasik <nasikahmd1@gmail.com>
2023-02-13 00:04:38 +0530

    front end css

---
+++ to-do-front-end/include/topNav.php
@@ -0,0 +1,34 @@
+<div class="bg-gradient-dark py-2">
+<nav
+  class="navbar navbar-expand-lg top-0 z-index-3 w-100 shadow-none my-3  navbar-transparent ">
+  <div class="container">
+    <a class="navbar-brand  text-white " href="" rel="tooltip" title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
+      <h3 style="color: white;">Todo</h3>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navigation">
+      <ul class="navbar-nav navbar-nav-hover ms-auto">
+        <div class="row">
+          <div class="col-auto m-auto">
+            <span style="color: white; font-weight:bold">User :</span>  
+          </div>
+          <div class="col-auto m-auto">
+            <span style="color: white; font-weight:bold">Nasik</span>  
+          </div>
+          <div class="col-auto m-auto"></div>
+          <div class="col-auto m-auto">
+            <div class="dropdown">
+              <a class="text-white cursor-pointer" title="logout" href="logout.php">
+                <i class="fa fa-sign-out"></i>
+              </a>              
+            </div>
+          </div>
+          
+        </div>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
